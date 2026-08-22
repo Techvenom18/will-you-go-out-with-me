ef033536a2d75cef617053959cb00fa7ab003f85
Techvenom18 <sumitrathore45528@gmail.com>
2026-08-23 02:36:41 +0530

    Update Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="site-footer">
-      <span>© 2026 Invitation. All rights reserved.</span>
+      <span>© 2026 Invitation, All rights reserved.</span>
       <span>Crafted &amp; Developed by Suprise Planner(S)</span>
     </footer>
   );
